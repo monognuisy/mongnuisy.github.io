fb9363035dc9bcb485daf23af5578c4bed8c2fb2
CHEMICALS1234 <ysmsmart1@gmail.com>
2021-03-06 22:09:59 +0900

    Added FrontPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="square-box" />
+      <div className="title-box">
+        <p>
+          welcome<span className="dot">.</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
