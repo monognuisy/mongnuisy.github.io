0dacc494a704895db051283eb840fdd4e43e9bd1
CHEMICALS1234 <ysmsmart1@gmail.com>
2021-08-14 11:15:07 +0900

    header, footer...

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="square-box" />
       <div className="title-box">
+        {/* <p className="title-imoji">😀Hi.</p> */}
         <p className="title-text">
-          in-development<span className="dot">.</span>
+          Sungmin Yoo<span className="title-text-dot">.</span>
         </p>
+      </div>
+      <div className="footer">
+        <a
+          href="https://chemicals1234.tistory.com/"
+          className="footer-items text"
+        >
+          blog
+        </a>
+        <a
+          href="https://github.com/CHEMICALS1234"
+          className="footer-items text"
+        >
+          github
+        </a>
+        <a
+          href="https://www.instagram.com/chemicals1234/"
+          className="footer-items text"
+        >
+          instagram
+        </a>
       </div>
     </div>
   );
