4ac98bc076f0893a8944cab62e14612c6f9acf6d
monognuisy <ysmsmart1@gmail.com>
2021-08-18 11:33:41 +0900

    changed location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <div className="header"></div>
@@ -34,6 +34,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
