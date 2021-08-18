df509e4cc199d21fd877fbd94fb2d815d7362e03
monognuisy <ysmsmart1@gmail.com>
2021-08-18 21:21:05 +0900

    docs: Move GlobalStyle to App.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import './index.css';
-import GlobalStyle from './Components/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
